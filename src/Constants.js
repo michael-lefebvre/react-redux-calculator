9c0082aead2685205f56106c627ba1ef406bbefd
Michael Lefebvre <michael@scenedata.com>
2018-04-26 01:36:15 +0200

    List some constants we will use across the App

---
+++ src/Constants.js
@@ -0,0 +1,21 @@
+
+//
+// Operators list
+// --------------------------------------------------
+
+export const OPERATORS = ['+','-','/','*']
+
+//
+// Buttons values
+// --------------------------------------------------
+
+export const VALUE_TOGGLE = 'VALUE_TOGGLE'
+export const VALUE_EQUAL  = '='
+export const VALUE_DOT    = '.'
+
+//
+// Operation's rows types
+// --------------------------------------------------
+
+export const OPERATION_INPUT    = 'OPERATION_INPUT'
+export const OPERATION_OPERATOR = 'OPERATION_OPERATOR'
